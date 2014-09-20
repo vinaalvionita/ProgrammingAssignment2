54897dc6c6c154a1d8bb1739f6fde7f7bd746ee4
vinaalvionita <vina.alvionita93@gmail.com>
2014-09-20 20:42:23 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,58 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-cache <- NULL
-setMatrix <- function(newValue) {
-        x <<- newValue
-        cache <<- NULL
-        }
- getMatrix <- function() {
- x
- }
- cacheInverse <- function(solve) {
- cache <<- solve
- }
- getInverse <- function() {
- cache
- }
- list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
- }
+makeCacheMatrix <- function(x = numeric()) {
+  cache <- NULL
+  setMatrix <- function(newValue) {
+      x <<- newValue
+      cache <<- NULL
+      }
+  getMatrix <- function() {
+  x
+  }
+  cacheInverse <- function(solve) {
+  cache <<- solve
+  }
+  getInverse <- function() {
+  cache
+  }
+  list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+  }
+  
+## cacheSolve
+##This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+##If the inverse has already been calculated (and the matrix has not changed),
+##then the cachesolve should retrieve the inverse from the cache.
 
-## cacheSolve <- function(x, ...) {
-## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(y, ...) {
-inverse <- y$getInverse()
-if(!is.null(inverse)) {
- message("getting cached data")
- return(inverse)
-}
-data <- y$getMatrix()
-inverse <- solve(data)
-y$cacheInverse(inverse)
- inverse
-}
-matrix(c(1,2,12,13), nrow = 2, ncol = 2)
-     [,1] [,2]
-[1,]    1   12
-[2,]    2   13
-a <- makeCacheMatrix( matrix(c(1,2,12,13), nrow = 2, ncol = 2) )
-summary(a)
-             Length Class  Mode    
-setMatrix    1      -none- function
-getMatrix    1      -none- function
-cacheInverse 1      -none- function
-getInverse   1      -none- function
-a$getMatrix()
-     [,1] [,2]
-[1,]    1   12
-[2,]    2   13
-cacheSolve(a)
-           [,1]        [,2]
-[1,] -1.1818182  1.09090909
-[2,]  0.1818182 -0.09090909
-cacheSolve(a)
-getting cached data
-           [,1]        [,2]
-[1,] -1.1818182  1.09090909
-[2,]  0.1818182 -0.09090909
+  inverse <- y$getInverse()
+  if(!is.null(inverse)) {
+  message("getting cached data")
+  return(inverse)
+  }
+  data <- y$getMatrix()
+  inverse <- solve(data)
+  y$cacheInverse(inverse)
+  inverse
+  }
+        ## Return a matrix that is the inverse of 'x'
